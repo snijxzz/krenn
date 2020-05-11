30e288b3eaedc4deb0d39700016d00958d302397
snijxzz <65198935+snijxzz@users.noreply.github.com>
2020-05-12 01:05:05 +0200

    Add files via upload

---
+++ assets/js.js
@@ -0,0 +1,50 @@
+(function(c, a, m) {
+    var title = (c || document.title) + " " + (a || "-") + " ";
+    setInterval(function() {
+      title = title.substring(1) + title.charAt(0);
+      document.title = title;
+    }, m || 300);
+  })(
+    "krenn.xyz ",
+    " ",
+    250
+  );
+      //Disable right-click + F12
+    window.onload = function() {
+    document.addEventListener("contextmenu", function(e){
+      e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(ma) {
+      // "I" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        disabledEvent(e);
+      }
+      // "J" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        disabledEvent(e);
+      }
+      // "S" key + macOS
+      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+      }
+      // "U" key
+      if (e.ctrlKey && e.keyCode == 85) {
+        disabledEvent(e);
+      }
+      // "F12" key
+      if (event.keyCode == 123) {
+        disabledEvent(e);
+  // document.onkeydown = function(tix) {
+      }
+    }, false);
+    function disabledEvent(e){
+      if (e.stopPropagation){
+        e.stopPropagation();
+      } else if (window.event){
+        window.event.cancelBubble = true;
+      }
+      e.preventDefault();
+      return false;
+    }
+  };
